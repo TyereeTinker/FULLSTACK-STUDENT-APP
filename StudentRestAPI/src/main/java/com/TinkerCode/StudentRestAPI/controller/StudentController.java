a64e1cd50a6534e2b76beb403165c41966e005ba
Tyeree A. Tinker <71784154+TyereeTinker@users.noreply.github.com>
2024-04-14 16:22:18 -0500

    Update StudentController.java

--- StudentRestAPI/src/main/java/com/TinkerCode/StudentRestAPI/controller/StudentController.java
+++ StudentRestAPI/src/main/java/com/TinkerCode/StudentRestAPI/controller/StudentController.java
@@ -1,4 +1,35 @@
 package com.TinkerCode.StudentRestAPI.controller;
 
+import com.TinkerCode.StudentRestAPI.model.Student;
+import com.TinkerCode.StudentRestAPI.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+    @PostMapping("/add")
+    public String add (@RequestBody Student student){
+        studentRepo.save(student);
+        return "New Student Added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> list(){
+        return studentRepo.findAll();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String remove(@PathVariable("id") int ID){
+        studentRepo.deleteById(ID);
+        return "Student removed";
+    }
+
+
 }
