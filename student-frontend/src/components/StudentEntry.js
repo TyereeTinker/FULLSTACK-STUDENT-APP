df0c2802c31dcc6bd48df55d3e0b5fe3a312365e
Tyeree A. Tinker <71784154+TyereeTinker@users.noreply.github.com>
2024-04-18 22:37:06 -0500

    Updated Entry

--- student-frontend/src/components/StudentEntry.js
+++ student-frontend/src/components/StudentEntry.js
@@ -1,36 +1,50 @@
 import React, { useState } from 'react'
 import './Style.css';
 
-import { Container, Paper, TextField, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+import { Container, Paper, TextField, Select, MenuItem, InputLabel, FormControl, Button } from '@mui/material';
 
 function StudentEntry() {
 const paperStyle={padding: "50px 30px", width: "600px", margin: "20px auto"}
 const genWidth={width: "200px"}
 
-const [Gender, setGender] = useState('');
+const [fname, setFname] = useState("");
+const [lname, setLname] = useState("");
+const [departId, setDepartId] = useState(0);
+const [email, setEmail] = useState("");
+const [Gender, setGender] = useState("");
 
+const genChange = (event) => {
+  setGender(event.target.value);
+};
+
+const addStudent = () => {
+  console.log(fname, lname, departId, email, Gender);
+}
 
 
   return (
     <Container>
       <Paper elevation={3} style={paperStyle}>
         <form>
-          <TextField id="outlined-basic" label="First Name" variant="outlined"/>
-          <TextField id="outlined-basic" label="Last Name" variant="outlined"/> 
+          <TextField id="outlined-basic" label="First Name" variant="outlined" onChange={((e) => {setFname(e.target.value)})}/>
+          <TextField id="outlined-basic" label="Last Name" variant="outlined" onChange={((e) => {setLname(e.target.value)})}/> 
 
           <br/> <br/>
 
           <FormControl style={genWidth}>
             <InputLabel id="genId"> Gender </InputLabel>
-            <Select labelId="genId" value={Gender} label="Gender">
-              <MenuItem value={10}>Ten</MenuItem>
-              <MenuItem value={20}>Twenty</MenuItem>
+            <Select labelId="genId" value={Gender} label="Gender" onChange={genChange}>
+              <MenuItem value={"female"}> FEMALE </MenuItem>
+              <MenuItem value={"male"}> MALE </MenuItem>
             </Select>
           </FormControl>
-          <TextField id="outlined-basic" label="DepartmentId" variant="outlined"/> 
+          <TextField id="outlined-basic" label="DepartmentId" variant="outlined" onChange={((e) => {setDepartId(e.target.value)})}/> 
           <br/> <br/>
 
-          <TextField id="outlined-basic" label="E-mail" variant="outlined" fullWidth/>
+          <TextField id="outlined-basic" label="E-mail" variant="outlined" fullWidth onChange={((e) => {setEmail(e.target.value)})}/>
+          <br/>  <br/>
+
+          <Button size='' variant='contained' onClick={addStudent}> ADD STUDENT </Button>
         </form>
       </Paper>
     </Container>
